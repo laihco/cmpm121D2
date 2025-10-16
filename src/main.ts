61c7813972fd4413f2821cc58f5b4dbab5ff0465
ansetty <156857224+laihco@users.noreply.github.com>
2025-10-16 19:11:23 +0000

    added non-interactive canvas  (step 1)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+document.title = "रंगोली";
+
+const canvas = document.createElement("canvas");
+canvas.id = "main-canvas";
+canvas.width = 256;
+canvas.height = 256;
+document.body.appendChild(canvas);
